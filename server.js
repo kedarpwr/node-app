1d47317b28e222263c8f673e732a804848a971a0
kedarpwr <kedar.pawar@infeon.in>
2022-04-21 18:20:56 +0530

    v2

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello GitAction Working');
+  res.send('Hello ECS Working');
 });
 
 app.listen(PORT, HOST);
